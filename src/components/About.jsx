21e7d3c24da67bd128aab24a0f6126638ad714d4
Alwin Sajan <116751380+Alwin-Sajan@users.noreply.github.com>
2024-06-04 19:10:38 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = ({ darkMode }) => {
                                 sociis arcu lorem porttitor.
                             </p>
                         
-                        <div className='flex items-center max-md:mr-8  gap-8'>
+                        <div className='lg:flex items-center max-md:mr-8 max-md:grid gap-8'>
                             <motion.button whileHover={{ scale: 1.3 }} whileTap={{ scale: 1.1 }}
                                 className={`text-white ${darkMode ? 'bg-[#ff5555]' : ' bg-black'} z-50 cursor-pointer w-40 h-14 flex items-center max-md:text-xl justify-center rounded mt-4 p-2`}>Get Started {<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
